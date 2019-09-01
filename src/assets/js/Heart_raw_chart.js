799cd9dc8d5375d76fc47f89cac0b30cc35d4a53
KIET1996 <vovankiet1108@gmail.com>
2019-09-01 19:25:13 +0700

    feature: home ui

---
+++ src/assets/js/Heart_raw_chart.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+    const dataHeart = [];
+     
+    const Chart = new CanvasJS.Chart("ChartContainer", {
+        zoomEnabled: true, // Dùng thuộc tính có thể zoom vào graph
+            title: {
+                text: "HEART BEAT GRAPH" // Viết tiêu đề cho graph
+                    },
+            toolTip: { // Hiển thị truường trên graph
+                shared: true
+                    },
+            axisX: {
+                title: "chart updates every 2 secs" // Chú thích cho trục X
+                    },
+            data: [{
+                type: "line", // Chọn kiểu dữ liệu đường
+                xValueType: "dateTime", // Cài đặt kiểu giá trị tại trục X là thuộc tính thời gian
+                showInLegend: true, // Hiển thị "temp" ở mục chú thích (legend items)
+                name: "heart beat",
+                dataPoints: dataHeart // Dữ liệu hiển thị sẽ lấy từ data
+                    }],
+                });
+        var yHeartVal = 0; // Biến lưu giá trị nhip tim theo trục Y
+        const updateInterval = 2000; // Thời gian cập nhật dữ liệu 2000ms = 2s
+        const time = new Date(); // Lấy thời gian hiện tại
+
+        const updateChart = function() {
+                   
+            // Gán giá trị từ localhost:8000/get vào textbox để hiển thị bước đi, calo
+            const fat_gramms    =   document.getElementById("fat_gramms");
+            const meters        =   document.getElementById("meters");
+            const steps         =   document.getElementById("steps");
+            const callories     =   document.getElementById("callories");
+            const heart_rate    =   document.getElementById("heart_rate");
+                        // Xuất ra màn hình console trên browser giá trị nhận được từ localhost:8000/get
+
+            time.setTime(time.getTime() + updateInterval);
+            yHeartVal = parseInt(Math.round((0+200)*150)/100);
+            dataHeart.push({ // cập nhât dữ liệu mới từ server
+                x: time.getTime(),
+                y: yHeartVal
+                    });
+
+            Chart.render(); // chuyển đổi dữ liệu của của graph thành mô hình đồ họa
+                    };
+
+            updateChart(); // Chạy lần đầu tiên
+            setInterval(function() { // Cập nhật lại giá trị graph sau thời gian updateInterval
+                updateChart()
+            }, updateInterval);
+            
+    }
---
+++ src/assets/js/function.js
@@ -0,0 +1,14 @@
+
+const   heart_rate_box   =    document.getElementById('heart_rate_box');
+const   heart_rate       =    document.getElementById('heart_rate');
+
+function changeStatusHeart(rate){
+    if (rate >100) {
+        heart_rate_box.style.backgroundColor='#ffb3b3';
+        heart_rate.style.backgroundColor='#ffb3b3';
+        }
+    else {
+        heart_rate_box.style.backgroundColor='white';
+        heart_rate.style.backgroundColor='white';
+    }
+};
