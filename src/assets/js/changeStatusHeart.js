686a44c8e9f33cca84b505e5871bd7be7f9aa0b9
Noat <nmtoan251998@gmail.com>
2019-09-03 11:35:06 +0700

    Merge dev/home-interface to master

--- src/assets/js/changeStatusHeart.js
+++ src/assets/js/changeStatusHeart.js
@@ -5,11 +5,11 @@ const   heart_rate       =    document.getElementById('heart_rate');
 export function changeStatusHeart(rate){
 	
     if (rate > 100) {
-         heart_rate_box.style.backgroundColor = '#ffb3b3';
-         heart_rate.style.backgroundColor = '#ffb3b3';
-         }
-     else {
-         heart_rate_box.style.backgroundColor = 'white';
-         heart_rate.style.backgroundColor = 'white';
-     }
+        heart_rate_box.style.backgroundColor = '#ffb3b3';
+        heart_rate.style.backgroundColor = '#ffb3b3';
+        }
+    else {
+        heart_rate_box.style.backgroundColor = 'white';
+        heart_rate.style.backgroundColor = 'white';
+    }
 };
