bd4bb3f3e2d2d600a72881f503cf5e4ba8535070
Noat <nmtoan251998@gmail.com>
2019-09-27 18:55:42 +0700

    Trying to merge dev/home-page

--- src/assets/js/getMac.js
+++ src/assets/js/getMac.js
@@ -1,5 +1,4 @@
 //Lấy danh sách địa chỉ Mac đã kết nối
-
 $(document).ready(function(){
   	$("#get-mac").click(function(){
   		listMac=['30:52:CB:86:B6:B0','CE:36:72:D8:4F:64', '9C:5C:F9:19:A5:DB','EC:79:71:75:7E:8F']
--- src/assets/js/rederData.js
+++ src/assets/js/rederData.js
@@ -26,8 +26,7 @@ window.onload = function() {
     const updateInterval = 2000; // Thời gian cập nhật dữ liệu 2000ms = 2s
     const time = new Date(); // Lấy thời gian hiện tại
 
-    const updateChart = function() {
-                   
+    module.exports.updateChart = function(data) {
         // Gán giá trị từ localhost:8000/get vào textbox để hiển thị bước đi, calo
         const fat_gramms    =   document.getElementById("fat_gramms");
         const meters        =   document.getElementById("meters");
@@ -43,7 +42,7 @@ window.onload = function() {
         fat_gramms.value    =   data.fat_gramms;
 
         changeStatusHeart(data.heart_rate, localStorage.getItem('old')); //changeStatusHeart(rate,old)
-            // Xuất ra màn hình console trên browser giá trị nhận được từ localhost:8000/get
+        // Xuất ra màn hình console trên browser giá trị nhận được từ localhost:8000/get
         time.setTime(time.getTime() + updateInterval);
         yHeartVal = parseInt(Math.round((0+200)*150)/100);
         dataHeart.push({ // cập nhât dữ liệu mới từ server
@@ -53,10 +52,4 @@ window.onload = function() {
 
         Chart.render(); // chuyển đổi dữ liệu của của graph thành mô hình đồ họa
     };
-
-    updateChart(); // Chạy lần đầu tiên
-    setInterval(function() { // Cập nhật lại giá trị graph sau thời gian updateInterval
-        updateChart()
-    }, updateInterval);
-            
 }
