7642acf2102b5285c82433d8b6a9847d18e4a146
KIET1996 <vovankiet1108@gmail.com>
2019-09-28 22:39:41 +0700

    resolving list mac

--- src/assets/js/getMac.js
+++ src/assets/js/getMac.js
@@ -2,12 +2,18 @@
 
 $(document).ready(function(){
   	$("#get-mac").click(function(){
+
   		listMac=['30:52:CB:86:B6:B0','CE:36:72:D8:4F:64', '9C:5C:F9:19:A5:DB','EC:79:71:75:7E:8F']
-		$('#list_mac').empty();
-  		listMac.forEach(item => {
-  			const output = '<li><a href="/data/start-python?add='+ item +'">'+item+'</a></li>';
+		  $('#list_mac').empty();
+  		  listMac.forEach(item => {
+  			const output = '<li><a id="mac_add" href="/data/start-python?add='+ item +'">'+item+'</a></li>';
   			$('#list_mac').append(output);
+
   		});
-  		$("li a").prop('disabled', true);
-  	});
+      $("a").addEventListener('click',(event)=>{
+          event.preventDefault();
+      });
+  		
+  	}); 	
 });
+  
