a33a4bbe2ce1a5e6b02ce6558c5670e0c4f83f47
Noat <nmtoan251998@gmail.com>
2019-10-03 18:37:40 +0700

    Working

---
+++ src/api/controllers/data.controller.js
@@ -0,0 +1,10 @@
+const httpStatus = require('http-status');
+const CronJob = require('cron').CronJob;
+
+module.exports.backup = (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+}
