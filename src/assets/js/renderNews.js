f49d575674b54f8bc5242fe0a384ae4481a071a7
KIET1996 <vovankiet1108@gmail.com>
2019-10-05 12:12:34 +0700

    Resolve display time in chart

---
+++ src/assets/js/renderNews.js
@@ -0,0 +1,13 @@
+       
+
+
+$("#news").on("", async (event) => {
+    event.preventDefault();
+    const renderNews = await axios({
+        method: 'get',
+        url: '/api/news/',
+       
+    });
+
+    console.log(renderNews);
+});
