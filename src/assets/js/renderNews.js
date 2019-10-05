890fa5facb4e6563e20e8095f56166bc6615e44e
KIET1996 <vovankiet1108@gmail.com>
2019-10-05 18:45:23 +0700

    add news

--- src/assets/js/renderNews.js
+++ src/assets/js/renderNews.js
@@ -1,13 +1,6 @@
        
 
 
-$("#news").on("", async (event) => {
-    event.preventDefault();
-    const renderNews = await axios({
-        method: 'get',
-        url: '/api/news/',
-       
-    });
-
-    console.log(renderNews);
+$(document).ready(function(){
+    	$("#news").append('<div class="col-lg-4 col-md-4 col-sm-6" ><img src='+ src + 'alt='+alt+'/><h4><a href='+href+'>'+title'+</a></h4></div>');
 });
