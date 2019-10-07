88d0a8ff2d9fe4f6102bcf0cf42af7149d033ad5
Noat <nmtoan251998@gmail.com>
2019-10-07 19:12:44 +0700

    feature: fag page

--- src/api/middlewares/errorHandler.middleware.js
+++ src/api/middlewares/errorHandler.middleware.js
@@ -1,7 +1,7 @@
 const httpStatus = require('http-status');
 
 module.exports.notFound = (req, res, next) => {
-    return res.status(httpStatus.OK).redirect('/404');
+    return res.status(httpStatus.NOT_FOUND).redirect('/404');
 }
 
 module.exports.errorHandler = (error, req, res, next) => {    
