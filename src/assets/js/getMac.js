6620ed0717a773989edaadb4619cc2e9b6c43321
KIET1996 <vovankiet1108@gmail.com>
2019-10-15 10:15:06 +0700

    resovle catch error get Mac

--- src/assets/js/getMac.js
+++ src/assets/js/getMac.js
@@ -11,29 +11,32 @@ export const renderConnectedMacAddresses = () => {
 				url: '/api/ble/mac'
 			});
 			// remove duplicate elements from array
-			const macAddresses = [...new Set([...connectedAddresses.data.addresses])];
-			$('#list_mac').empty();
-			macAddresses.forEach(item => {
-				const output = '<li><a class="mac_add" href="/data/start-python?add=' + item + '">' + item + '</a></li>';
-				$('#list_mac').append(output);
-			});
+			if (connectedAddresses.status === 200) {
+				HandleResponse('.mac-response', connectedAddresses.data.msg, connectedAddresses.status);
+				const macAddresses = [...new Set([...connectedAddresses.data.addresses])];
+				$('#list_mac').empty();
+				macAddresses.forEach(item => {
+					const output = '<li><a class="mac_add" href="/data/start-python?add=' + item + '">' + item + '</a></li>';
+					$('#list_mac').append(output);
+				});
 
-			$(".mac_add").on('click', async (event) => {
-				event.preventDefault();
+				$(".mac_add").on('click', async (event) => {
+					event.preventDefault();
 
-				// leave this to me
-				const macAddPattern = /(([a-zA-Z0-9]{2}:){5})([a-zA-Z0-9]{2})/;
-				const macAdd = event.target.textContent.match(macAddPattern)[0].trim();
+					// leave this to me
+					const macAddPattern = /(([a-zA-Z0-9]{2}:){5})([a-zA-Z0-9]{2})/;
+					const macAdd = event.target.textContent.match(macAddPattern)[0].trim();
 
-				// const startPythonResult = await axios({
-				// 	method: 'get',
-				// 	url: '/api/ble/start-python',
-				// 	params: {
-				// 		add: macAdd,
-				// 		socketID: socketId
-				// 	}
-				// });
-			});
+					// const startPythonResult = await axios({
+					// 	method: 'get',
+					// 	url: '/api/ble/start-python',
+					// 	params: {
+					// 		add: macAdd,
+					// 		socketID: socketId
+					// 	}
+					// });
+				});
+			}
 		} catch (error) {
 			if (error.response.data.msg) {
 				HandleResponse('.mac-response', error.response.data.msg, error.response.status);
