0f54edee4c74f36820b745b42d0ccc264fe8fc70
KIET1996 <vovankiet1108@gmail.com>
2019-10-18 20:06:50 +0700

    fix id error

--- src/assets/js/getNews.js
+++ src/assets/js/getNews.js
@@ -10,19 +10,18 @@ $(document).ready(async function(){
 			url: '/api/news'
 		});
 
-		 if (crawnews.status === 200) {
-            HandleResponse('.news-response', crawnews.data.msg, crawnews.status);
-			$('#list_news').empty();
+		$('#list_news').empty();
 			console.log(crawnews);
 			crawnews.data.forEach(item => {
 		//		console.log(item);
 				const output = 	'<div class="col-lg-4 col-md-4 col-sm-4"><div class="card" style="width: 18rem;"><img class="card-img-top" src=' + item.src + 'alt=' +item.alt + ' > <div class="card-body"> <h4>'+item.title+'</h4><a href=' + item.href + ' class="btn btn-primary">Xem bài viết</a></div></div></div><br>';
 				$('#list_news').append(output); 
-			});
+			});	
+
+		 if (crawnews.status === 404) {
+            HandleResponse('.news-response', crawnews.data.msg, crawnews.status);
 		}
-		else {
-			HandleResponse('.news-response', crawnews.data.msg, crawnews.status);
-		}	
+			
 		
 	} catch (error) {
 		if (error.response.data.msg) {
