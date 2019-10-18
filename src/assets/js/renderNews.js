7a871accfca746f0a3ed1025af551d223612ccac
Noat <nmtoan251998@gmail.com>
2019-10-18 18:08:28 +0700

    ?

--- src/assets/js/renderNews.js
+++ src/assets/js/renderNews.js
@@ -1,13 +1,8 @@
-       
-
-
 $("#news").on("", async (event) => {
     event.preventDefault();
     const renderNews = await axios({
         method: 'get',
         url: '/api/news/',
-       
-    });
-
+    });    
     console.log(renderNews);
 });
