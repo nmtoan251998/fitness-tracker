bc6b5e47aa3faf78addd2ca88741819f74816ecb
KIET1996 <vovankiet1108@gmail.com>
2019-11-04 19:51:10 +0700

    fix display device

--- src/assets/js/getInfor.js
+++ src/assets/js/getInfor.js
@@ -3,20 +3,30 @@ import axios from 'axios';
 
 $(document).ready(async function(){
 		
-		try {      
-			const getInfor = await axios({
-				method: 'get',
-				url: '/api/ble/device/:mac'
-			});
-	
-			if (getInfor.status === 200) {
-				$('#device-serial').empty();
-				$('#device-add').empty();
-				const serial 	= document.getElementById('device-serial');
-				const add 		= document.getElementById('device-add');
-				serial.value 	= getInfor.data.serial;
-				add.value 		= getInfor.data.macAdd;
-			}			
+		try {    
+
+			const mac = document.getElementById('mac-add');  
+			if (mac.value !== '') {
+
+				const getInfor = await axios({
+					method: 'get',
+					url: '/api/ble/device/'+mac.value
+				});
+		
+				console.log(getInfor);
+				if (getInfor.status === 200) {
+					$('#device-serial').empty();
+					$('#device-add').empty();
+					const serial 	= document.getElementById('device-serial');
+					const add 		= document.getElementById('device-add');
+					serial.value 	= getInfor.data.serial;
+					add.value 		= getInfor.data.macAdd;
+				}		
+
+				if (getInfor.status === 404) {
+					HandleResponse('.news-response', getInfor.data.msg, getInfor.status);
+				}	
+			}
 		
 		} catch (error) {
 			// handle error response
